ef0f7d4bea2b882bef4ca9d9d4d3165de463333b
MarJose-Car <maria-jose.carvalho@lepoles.com>
2020-02-05 16:27:00 +0100

    partie1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormsModule} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pendu';
+export class AppComponent implements OnInit {
+  //propriétés d'instance
+  dictionnaire: string[] = [
+    "table",
+    "maison",
+    "chaise",
+    "cuisine",
+    "chateau"
+  ];
+  mot_a_trouver: string [];
+  mot_a_afficher: string [];
+  proposition: string;
+  essais: number = 10;
+  message: string;
+
+
+  ngOnInit() {
+    //choisir un mot parmi les mots du dictionnaire
+    let hasard = Math.floor(Math.random() * this.dictionnaire.length);
+    this.mot_a_trouver = Array.from(this.dictionnaire[hasard]);
+    this.mot_a_afficher = Array(this.dictionnaire[hasard].length).fill('_');
+  }
+  verifier() {
+
+    if(this.essais === 0) {
+      this.message = "PERDU ! Le mot à trouver était : "+ this.mot_a_trouver.join('');
+    } else {
+    for(let i=0; i<this.mot_a_trouver.length; i++) {
+      if(this.proposition === this.mot_a_trouver[i]) {
+        this.mot_a_afficher[i] = this.proposition;
+      }
+    }
+    this.essais -= 1;
+  }
+    this.proposition ='';
+  }
 }
